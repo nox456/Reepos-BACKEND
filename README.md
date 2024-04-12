5455b7d58d77729a494768e5c5260d89f6ebc34a
Nox <nox030705@gmail.com>
2024-04-12 01:07:40 -0400

    chore: README, created and imported index router

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repos App Backend
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { HOST, PORT } from "./config/env.js";
 import morgan from "./middlewares/morgan.js"
+import routes from "./routes/index.routes.js"
 
 console.clear();
 const app = express();
@@ -9,6 +10,10 @@ const app = express();
 app.use(express.json())
 app.use(morgan)
 
+// Routes
+app.use(routes)
+
+// Start Server
 app.listen(PORT, HOST, () => {
     console.log(`Server on http://${HOST}:${PORT}`);
 });
