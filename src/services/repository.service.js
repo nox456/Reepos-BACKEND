96c5138724b3feaccaaab7c5d9f491777b371e32
Gabriel_Vieira <nox030705@gmail.com>
2024-06-05 20:28:30 -0400

    feat(services): save modifications in database

--- src/services/repository.service.js
+++ src/services/repository.service.js
@@ -9,30 +9,31 @@ import Repository_Language from "../models/repository_language.model.js";
 import projectInfo from "../lib/getProjectsInfo.js"
 
 export default class RepositoryService {
+    // Create a repostory with their commits, contributors, files, branches, languages and modifications
     static async createRepository(repoData, projectName) {
         const { commits, files, branches, contributors, modifications } = await projectInfo(projectName)
         const { name, description, user_owner, languages } = repoData
+        // Create repository in database
         const repoSaved = await Repository.save({ name, description, user_owner })
 
+        // Relate languages with repository
         for (const language of languages) {
             await Repository_Language.save(repoSaved.id, language)
         }
 
+        // Save the contributors of the repository in database
         const contributorsSaved = []
         for (const contributor of contributors) {
-            contributorsSaved.push(await Contributor.save({ contributor, repo: repoSaved.id }))
+            contributorsSaved.push(await Contributor.save({ name: contributor, repo: repoSaved.id }))
         }
 
+        // Save branches of the repository in database
         const branchesSaved = []
         for (const branch of branches) {
             branchesSaved.push(await Branch.save({ name: branch.name, type: branch.type, repo: repoSaved.id }))
         }
 
-        const filesSaved = []
-        for (const file of files) {
-            filesSaved.push(await File.save({ name: file.name, size: file.size, path: file.path, repo: repoSaved.id }))
-        }
-
+        // Save commits of the repository in database
         const commitsSaved = []
         for (const commit of commits) {
             const contributor = contributorsSaved.find((c) => c.name == commit.author).id
@@ -40,17 +41,39 @@ export default class RepositoryService {
 
             commitsSaved.push(commitSaved)
 
-            const branchesSaved = branchesSaved.filter((b) => commit.branches.includes(b.name))
+            const commitBranchesSaved = branchesSaved.filter((b) => commit.branches.includes(b.name))
 
-            for (const branchSaved of branchesSaved) {
+            // Relate commits with branches
+            for (const branchSaved of commitBranchesSaved) {
                 await Commit_Branch.save(commitSaved.id, branchSaved.id)
             }
         }
 
-        for (const modification of modifications) {
+        // Save files of the repository in database
+        const filesSaved = []
+        const modificationsSaved = []
+        for (const file of files) {
+            const fileSaved = await File.save({ name: file.name, size: file.size, path: file.path, repo: repoSaved.id })
+            filesSaved.push(fileSaved)
+
+            // Save modifications in database
+            const fileModifications = modifications.filter(m => m.file == file.path)
+
+            for (const fileModification of fileModifications) {
+                const commit = commitsSaved.find((c) => c.hash == fileModification.commit).id
+                modificationsSaved.push(await Modification.save({ type: fileModification.type, commit, file: fileSaved.id }))
+            }
+        }
+        // Save files deleted previously in database
+        const deletedFilesModifications = modifications.filter((m) => !filesSaved.some(f => f.path == m.file))
+        for (const modification of deletedFilesModifications) {
             const commit = commitsSaved.find((c) => c.hash == modification.commit).id
-            const file = filesSaved.find((f) => f.path == modification.file).id
-            await Modification.save({ type: modification.type, commit, file })
+
+            const file_name = modification.file.slice(modification.file.lastIndexOf("/") + 1)
+
+            const fileSaved = await File.save({name: file_name, size: "N/A", path: modification.file, repo: repoSaved.id})
+
+            modificationsSaved.push(await Modification.save({type: modification.type, commit, file: fileSaved.id}))
         }
         return repoSaved
     }
