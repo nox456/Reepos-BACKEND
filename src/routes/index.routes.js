20d0527bc8dad5d4504f21155e8ab53888767906
Gabriel_Vieira <nox030705@gmail.com>
2024-06-27 01:50:18 -0400

    fix: merge conflicts

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import { Router } from "express";
+import express from "express";
 import authRoutes from "./auth.routes.js"
 import userRoutes from "./user.routes.js"
 import repositoryRoutes from "./repository.routes.js"
 import fileRoutes from "./file.routes.js"
-=======
-import express from "express";
-import authRoutes from "./auth.routes.js";
-import userRoutes from "./user.routes.js";
-import repositoryRoutes from "./repository.routes.js";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
->>>>>>> feature/download-repository
 
 const downloadsDir = join(
     dirname(fileURLToPath(import.meta.url)),
@@ -25,14 +18,10 @@ router.use("/auth", authRoutes);
 // Base URL of users service
 router.use("/users", userRoutes);
 // Base URL of repositories routes
-<<<<<<< HEAD
 router.use("/repositories", repositoryRoutes)
 // Base URL of files routes
 router.use("/files", fileRoutes)
-=======
-router.use("/repositories", repositoryRoutes);
 // Serve downloads directory
 router.use(express.static(downloadsDir));
->>>>>>> feature/download-repository
-
+//
 export default router;
