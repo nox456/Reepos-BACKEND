fe2843dc376ef2f9c9e09639a745b98c3805a8e6
Gabriel_Vieira <nox030705@gmail.com>
2024-07-28 10:21:10 -0400

    core(files): better service responses

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -12,22 +12,19 @@ export default class FileController {
             console.error(e);
             return new ErrorHandler(res).internalServer();
         }
-        if (result?.validationError) {
-            return new ErrorHandler(res).badRequest(
-                result.validationError,
-                result.validationField,
-            );
-        } else if (result?.fileNotExistsDb) {
-            return new ErrorHandler(res).notFound(
-                "File doesn't exists in database!",
-                id,
-            );
-        } else if (result?.fileNotExistsCloud) {
-            return new ErrorHandler(res).notFound(
-                "File doesn't exists in cloud!",
-                id,
-            );
+        if (!result.success) {
+            if (result.error.type == "validation")
+                return new ErrorHandler(res).badRequest(
+                    result.error.message,
+                    null,
+                );
+            if (result.error.type == "not found")
+                return new ErrorHandler(res).notFound(
+                    result.error.message,
+                    null,
+                );
+        } else {
+            return ResponseHandler.ok("File founded!", result.data, res);
         }
-        return ResponseHandler.ok("File founded!", result, res);
     }
 }
