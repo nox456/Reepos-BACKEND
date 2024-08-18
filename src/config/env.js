902383ea129c505c0e1c847c46a02c1b25844043
Gabriel_Vieira <nox030705@gmail.com>
2024-08-18 01:03:18 -0400

    fix: removed errors from HOST and PORT env vars

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,4 @@
 // Throw errors when a needed env var doesn't exists
-if (!process.env.HOST) {
-    throw new Error("Env var HOST is not defined!");
-}
-if (!process.env.PORT) {
-    throw new Error("Env var PORT is not defined!");
-}
 if (!process.env.DB_URL) {
     throw new Error("Env var DB_URL is not defined!");
 }
