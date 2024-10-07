4b87fe88f7871863721ad90769b28c4d80f8dbdd
Gabriel_Vieira <nox030705@gmail.com>
2024-10-07 12:19:35 -0400

    fix(lib): initialize error object

--- src/lib/serviceError.js
+++ src/lib/serviceError.js
@@ -1,6 +1,7 @@
 export default class ServiceError {
     constructor(message,type) {
         this.success = false
+        this.error = {}
         this.error.message = message
         this.error.type = type
         this.data = null
