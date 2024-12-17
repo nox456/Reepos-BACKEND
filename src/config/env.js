fed8e43378786e0506fc7668c12081d4fe8b393f
Gabriel Vieira <nox030705@gmail.com>
2024-12-17 17:12:04 -0400

    perf: destructure the process.env object to better performance

--- src/config/env.js
+++ src/config/env.js
@@ -9,23 +9,43 @@ const requiredEnvVars = [
     'COOKIES_SAMESITE',
     'COOKIES_SECURE',
     'HOST',
-    'PORT'
+    'PORT',
 ]
 
 requiredEnvVars.forEach(varName => {
-    if (!process.env[varName]) {
+    if (!process.env[varName] && varName == 'HOST' && process.env.NODE_ENV != 'production') {
         throw new Error(`Required environment variable not defined: ${varName}`)
     }
 })
 
-export const HOST = process.env.HOST
-export const PORT = process.env.PORT
-export const DB_URL = process.env.DB_URL
-export const SECRET = process.env.SECRET
-export const SUPABASE_URL = process.env.SUPABASE_URL
-export const SUPABASE_KEY = process.env.SUPABASE_KEY
-export const SUPABASE_IMAGE_BUCKET = process.env.SUPABASE_IMAGE_BUCKET
-export const SUPABASE_REPOSITORY_BUCKET = process.env.SUPABASE_REPOSITORY_BUCKET
-export const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN
-export const COOKIES_SAMESITE = process.env.COOKIES_SAMESITE
-export const COOKIES_SECURE = process.env.COOKIES_SECURE
+const {
+    HOST,
+    PORT,
+    DB_URL,
+    SECRET,
+    SUPABASE_URL,
+    SUPABASE_KEY,
+    SUPABASE_IMAGE_BUCKET,
+    SUPABASE_REPOSITORY_BUCKET,
+    FRONTEND_ORIGIN,
+    COOKIES_SAMESITE,
+    COOKIES_SECURE,
+    NODE_ENV,
+    RENDER_EXTERNAL_HOST,
+} = process.env
+
+export {
+    HOST,
+    PORT,
+    DB_URL,
+    SECRET,
+    SUPABASE_URL,
+    SUPABASE_KEY,
+    SUPABASE_IMAGE_BUCKET,
+    SUPABASE_REPOSITORY_BUCKET,
+    FRONTEND_ORIGIN,
+    COOKIES_SAMESITE,
+    COOKIES_SECURE,
+    NODE_ENV,
+    RENDER_EXTERNAL_HOST,
+}
